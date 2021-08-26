ebfe767a395173000ff6858a7500ec252dcfc67b
Nathan Dixon <nathan.dixon@evadon.com>
2021-08-26 14:13:32 +0100

    Added query string option to show github credentials in LwM2M registry

--- Scripts/config.js
+++ Scripts/config.js
@@ -6,5 +6,5 @@ const config = {
   testUrlPrefix: 'test',
   devBranchName: 'test',
   devUrlPrefix: 'localhost',
-  showBranchSelectionQueryParameter: 'show',
+  showBranchSelectionQueryString: 'show',
 };
